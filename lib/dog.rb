b5a86e8c88eb7117b76521604690f2937824d5e9
Jeremy Emata <jeremy.emata@gmail.com>
2018-11-06 09:20:24 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,35 @@
+class Dog 
+  attr_accessor :name, :breed, :id 
+  
+  def initialize(hash)
+    @name = hash[:name]
+    @breed = hash[:breed]
+  end 
+  
+  def self.create_table 
+    sql = <<-SQL 
+            CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        breed TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table
+        sql = <<-SQL 
+    DROP TAble dogs 
+    SQL
+    DB[:conn].execute(sql)
+  end 
+  
+  def save
+    sql = <<-SQL 
+      INSERT INTO dogs (name, breed) VALUES (?,?)
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.breed)
+  end 
+  
+end 
