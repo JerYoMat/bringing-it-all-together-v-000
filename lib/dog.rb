35bc459bfd4d450b4be585c6a54ecd84aa8699fe
Jeremy Emata <jeremy.emata@gmail.com>
2018-11-06 09:35:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,4 +56,17 @@ class Dog
     
   end 
   
+  def find_by_name(name)
+    sql= <<-SQL 
+    Select * from dogs where name = ?
+    SQL
+    
+   data = DB[:conn].execute(sql, name).first 
+   hash_for_create = {:name => data[1],
+   :breed => data[2]}
+   new_dog = self.create(hash_for_create)
+   new_dog.id = data[0]
+   new_dog
+  end 
+  
 end 
